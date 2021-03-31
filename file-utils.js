166a35e1674094ed251a850ed8d63f2c466fc3f5
Radan Ganchev <radan.ganchev@gmail.com>
2021-03-31 09:38:27 +0300

    Use map instead of loop

--- file-utils.js
+++ file-utils.js
@@ -2,11 +2,8 @@ import fs from 'fs';
 
 export class FileUtils {
   concat(files) {
-    let result = '';
-    for (let i = 0; i < files.length; i++) {
-      result += fs.readFileSync(files[i]);
-    }
-
-    return result;
+    return files
+      .map(file => fs.readFileSync(file))
+      .join('');
   }
 }
